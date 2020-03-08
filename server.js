5b8ff745c02b2daf28a14352e80d326efc334b3d
lattrellsapon <lattrellsapon@gmail.com>
2020-03-08 16:08:30 +1300

    First commit

---
+++ server.js
@@ -0,0 +1,84 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const nodemailer = require('nodemailer');
+const creds = require('./config/config');
+
+const app = express();
+
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  next();
+});
+
+const transport = {
+  host: 'smtp.gmail.com',
+  port: 587,
+  secure: false,
+  auth: {
+    user: creds.USER,
+    pass: creds.PASS
+  }
+};
+
+const transporter = nodemailer.createTransport(transport);
+
+transporter.verify((error, success) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('Server is ready to take messages');
+  }
+});
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+// parse application/json
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+app.post('/send', (req, res) => {
+  //   res.send('POST REQUEST WENT HERE');
+  //   console.log('post post post');
+  //   console.log(req.body.username);
+
+  const username = req.body.username;
+  const email = req.body.email;
+  const message = req.body.message;
+  const content = `${username} - ${email} wants to say: 
+    
+        ${message}
+
+    `;
+
+  const mail = {
+    from: username,
+    to: 'lattrellsapon@gmail.com',
+    subject: 'Treezy Web Design | New user message',
+    text: content
+  };
+
+  transporter.sendMail(mail, (err, data) => {
+    if (err) {
+      res.json({
+        msg: 'fail'
+      });
+    } else {
+      res.json({
+        msg: success
+      });
+    }
+  });
+});
+
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running port ${PORT}`);
+});
